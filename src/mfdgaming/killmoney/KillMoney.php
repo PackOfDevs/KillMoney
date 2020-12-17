2d52687320d454e572da43c7b722d1cf4bdfa3f0
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-17 23:56:23 +0200

    Update KillMoney.php

--- src/mfdgaming/killmoney/KillMoney.php
+++ src/mfdgaming/killmoney/KillMoney.php
@@ -3,8 +3,8 @@
        // |  \/  |  ___|  _ \ / ___| __ _ _ __ ___ (_)_ __   __ _ 
       //  | |\/| | |_  | | | | |  _ / _` | '_ ` _ \| | '_ \ / _` |
      //   | |  | |  _| | |_| | |_| | (_| | | | | | | | | | | (_| |
-    //    |_|  |_|_|   |____/ \____|\__,_|_| |_| |_|_|_| |_|\__, |                                                    |___/ 
-   //
+    //    |_|  |_|_|   |____/ \____|\__,_|_| |_| |_|_|_| |_|\__, |
+   //                                                        |___/ 
   //      Licensed under the Apache License, Version 2.0 (the "License")
  //       you may not use this file except in compliance with the License.
 //        You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
